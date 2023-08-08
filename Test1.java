dba23e11f70a0bf0b09eeae5288d71ac3ef1cbef
shashikala <chandrasekaremsmh@gmail.com>
2023-08-08 23:50:26 +0530

    Initial commit

---
+++ Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int i = 0;
+        int k = 1;
+        while (i++ < 5) {
+            int j = 0;
+            while (j++ < k){
+                System.out.print("+ ");
+                    
+            }
+            System.out.println();
+            k++;
+        }
+    }
+    
+}
---
+++ assignment4.java
@@ -0,0 +1,101 @@
+public class assignment4 {
+    public static void main(String[] args) {
+                /* 
+            Single Loop, Nested Loops
+            Via +
+            + + + + +
+            + + + +
+            + + +
+            + +
+            +                           
+            + +
+            + + +
+            + + + +
+            + + + + +
+         */
+
+        
+        int y = 0;
+        int z = 5;
+
+        while (y++ < 5){
+
+            int x = 0;
+            while (x++ < z){
+                if (z == 1) {
+                    int i = 0;
+                    int k = 1;
+                    while (i++ < 5) {
+                        int j = 0;
+                        while (j++ < k){
+                            System.out.print("+ ");
+                        }
+                        System.out.println();
+                        k++;
+                    }
+           
+            
+                }else System.out.print("+ ");
+            }
+            System.out.println();
+            z--;
+        }    
+        
+        
+
+         /*
+            Single Loop, Nested Loops
+            Via +
+            + + + + + + +
+            + + +   + + +
+            + +       + +
+            +           +
+          */
+
+         /*
+            Nested Loops
+            Via + (Space)
+            + + + + + + +
+            + + +   + + +
+            + +       + +
+            +           +
+            + +       + +
+            + + +   + + +
+            + + + + + + +
+          */          
+
+         /* 
+            Nested Loops
+            Via + / \
+            + + + + + +
+            + + / \ + +
+            + / / \ \ +
+            + \ \ / / +
+            + + \ / + +
+            + + + + + +
+          */
+
+          /*
+            Nested Loops
+            Via + / \
+            + + + + + 
+            + / + \ +
+            + + + + +
+            + \ + / +
+            + + + + +
+           */
+
+           /*
+            Nested Loops
+            Via + Space
+                + 
+              + + +
+            + + + + + 
+              + + + 
+                + 
+            */
+
+
+
+    }
+}
